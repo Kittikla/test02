29f9e259fcdd4be03c0251d29051adbec15e500b
Unknow248 <tonkla.tonkla248@gmail.com>
2020-04-17 12:59:53 +0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int number1 = scan.nextInt();
+        int number2 = scan.nextInt();
+        int number3 = scan.nextInt();
+        float number4 = scan.nextFloat();
+        float number5 = scan.nextFloat();
+        System.out.println((number1+number2+number3)/(number4+number5));
+    }
+}
